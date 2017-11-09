0caf43094b670756bee904227de7f006e0ff738b
Samitha Jayathilake <general@macbook02.local>
2017-11-09 10:03:47 +0100

    commitTest1 AuthorAmend

--- JDMowerApp/ViewController.swift
+++ JDMowerApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //First change for commits, some more change
+        //First change for commits, some more change m ore
         
     }
 
